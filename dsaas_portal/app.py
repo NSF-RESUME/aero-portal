a9903466a1cd83e786e7813506026bfd733c2474
valhayot <valeriehayot@gmail.com>
2024-03-24 18:58:15 -0500

    Add Django interface

---
+++ dsaas_portal/app.py
@@ -0,0 +1,58 @@
+
+from django.apps import AppConfig
+from dsaas_portal import checks
+
+class Dsaasportal(AppConfig):
+    name = 'DSaaS'
+
+
+
+SEARCH_INDEXES = {
+    "dsaas_portal": {
+        "uuid": "ef085c4b-7ce8-4955-b22f-8c907b690abb",
+        "name": "DSaaS",
+        "template_override_dir": "dsaas_portal",
+        "facets": [
+            {
+                'name': 'Name',
+                'field_name': 'name'
+            },
+            {
+                'name': 'Description',
+                'field_name': 'description',
+            },
+            {
+                'name': 'Email',
+                'field_name': 'email',
+            },
+            {
+                'name': 'Tags',
+                'field_name': 'tags',
+            },
+            {
+                'name': 'Source Name',
+                'field_name': 'source',
+            },
+            {
+                'name': 'Source ID',
+                'field_name': 'source_id',
+            },
+            {
+                'name': 'Version',
+                'field_name': 'version'
+            },
+            {
+                'name': 'Checksum',
+                'field_name': 'checksum'
+            },
+            {
+                'name': 'File Size',
+                'field_name': 'file_size'
+            },
+            {
+                'name': 'Date Created',
+                'field_name': 'date_created'
+            }
+        ],
+    }
+}
